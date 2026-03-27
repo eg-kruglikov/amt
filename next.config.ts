1e81115a8d1df35cab6ea86ad37e46e9e59cc5d6
shluse <shluse>
2026-03-27 19:43:17 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: "standalone",
+} satisfies NextConfig;
 
 export default nextConfig;
